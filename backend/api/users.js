dde1c1dca83c7730960738fa34d895b71b94b734
Branden Sherman <bshermandeveloper@gmail.com>
2020-11-03 15:37:39 -0500

    feat(API): added /api/users routes, user authorization and authentication with jwt, passwords hashed

---
+++ backend/api/users.js
@@ -0,0 +1,102 @@
+const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { User } = require('../db');
+const { generateToken } = require('../utils/generateToken');
+const { protect } = require('../middleware/authMiddleware');
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+router.get(
+  '/profile',
+  protect,
+  asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      const { _id, name, email, isAdmin } = user;
+
+      res.json({
+        _id,
+        name,
+        email,
+        isAdmin,
+      });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
+    }
+
+    res.send('success');
+  })
+);
+
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      const { _id, name, email, isAdmin } = user;
+      const token = generateToken(_id);
+
+      res.status(201).json({
+        _id,
+        name,
+        email,
+        isAdmin,
+        token,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
+  })
+);
+
+// @desc Login user
+// @route POST /api/users/login
+// @access Public
+router.post(
+  '/login',
+
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      const { _id, name, email, isAdmin } = user;
+      const token = generateToken(_id);
+
+      res.json({
+        _id,
+        name,
+        email,
+        isAdmin,
+        token,
+      });
+    } else {
+      res.status(401);
+      throw new Error('Invalid email or password');
+    }
+  })
+);
+
+module.exports = router;
--- backend/db/user.js
+++ backend/db/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userSchema = mongoose.Schema(
+const UserSchema = mongoose.Schema(
   {
     name: {
       type: String,
@@ -27,4 +28,18 @@ const userSchema = mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model('User', userSchema);
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  // This will only run if the password was modified
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
