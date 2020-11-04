373091d15013a838e882add626b1abe0085b877c
Branden Sherman <bshermandeveloper@gmail.com>
2020-11-04 10:21:56 -0500

    feat(frontend auth): implemented user login/logout functionality

---
+++ frontend/src/reducers/userReducer.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+const USER_LOGIN_REQUEST = 'USER_LOGIN_REQUEST';
+const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS';
+const USER_LOGIN_FAIL = 'USER_LOGIN_FAIL';
+const USER_LOGOUT = 'USER_LOGOUT';
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
+};
+
+// User Reducer
+export const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return { loading: true, ...state };
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,19 +5,25 @@ import loggingMiddleware from 'redux-logger';
 import { productListReducer } from './reducers/productListReducer';
 import { productDetailsReducer } from './reducers/productDetailsReducer';
 import { cartReducer } from './reducers/cartReducer';
+import { userReducer } from './reducers/userReducer';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  user: userReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
 
 const intitialState = {
   cart: { cartItems: cartItemsFromStorage },
+  user: { userInfo: userInfoFromStorage },
 };
 
 // const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware));
